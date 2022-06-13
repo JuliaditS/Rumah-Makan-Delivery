fbfb5c80558ea1ba84ba352e1431aed43adcfb47
JuliaditS <juliaditsyahputra@gmail.com>
2022-06-13 18:50:49 +0700

    First Prototype

---
+++ Assets/Scripts/Splashscreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Splashscreen : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public int goToScene;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        videoPlayer.loopPointReached += Endvid;
+    }
+
+    public void Endvid(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(goToScene);
+    }
+}
